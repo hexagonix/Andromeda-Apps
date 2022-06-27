dba0b964cf90f92c6f6743cb5f483e473e02edcb
Felipe Lunkes <felipemiguel_nery@hotmail.com>
2022-06-27 13:18:59 -0300

    v2.0.2

--- Configurações/Dados/versao.asm
+++ Configurações/Dados/versao.asm
@@ -15,6 +15,6 @@
 ;;
 ;;************************************************************************************
 
-VERSAOCONFIG             equ "2.0.1"
+VERSAOCONFIG             equ "2.0.2"
 VERSAOFERRAMENTAS        equ "1.2"
 VERSAODESIGNADAANDROMEDA equ "H1 R4"
