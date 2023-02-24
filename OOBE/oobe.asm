c4b65737dbc73327886d74dbba8ef727c516d385
Felipe Lunkes <felipenldev@gmail.com>
2023-02-24 04:26:58 -0300

    v1.0
    New OOBE experience for first time boot added to Hexagonix

---
+++ OOBE/oobe.asm
@@ -0,0 +1,178 @@
+;;*************************************************************************************************
+;;
+;; 88                                                                                88              
+;; 88                                                                                ""              
+;; 88                                                                                                
+;; 88,dPPPba,   ,adPPPba, 8b,     ,d8 ,adPPPPba,  ,adPPPb,d8  ,adPPPba,  8b,dPPPba,  88 8b,     ,d8  
+;; 88P'    "88 a8P     88  `P8, ,8P'  ""     `P8 a8"    `P88 a8"     "8a 88P'   `"88 88  `P8, ,8P'   
+;; 88       88 8PP"""""""    )888(    ,adPPPPP88 8b       88 8b       d8 88       88 88    )888(     
+;; 88       88 "8b,   ,aa  ,d8" "8b,  88,    ,88 "8a,   ,d88 "8a,   ,a8" 88       88 88  ,d8" "8b,   
+;; 88       88  `"Pbbd8"' 8P'     `P8 `"8bbdP"P8  `"PbbdP"P8  `"PbbdP"'  88       88 88 8P'     `P8  
+;;                                               aa,    ,88                                         
+;;                                                "P8bbdP"       
+;;
+;;            Sistema Operacional Hexagonix® - Hexagonix® Operating System            
+;;
+;;                  Copyright © 2015-2023 Felipe Miguel Nery Lunkes
+;;                Todos os direitos reservados - All rights reserved.
+;;
+;;*************************************************************************************************
+;;
+;; Português:
+;; 
+;; O Hexagonix e seus componentes são licenciados sob licença BSD-3-Clause. Leia abaixo
+;; a licença que governa este arquivo e verifique a licença de cada repositório para
+;; obter mais informações sobre seus direitos e obrigações ao utilizar e reutilizar
+;; o código deste ou de outros arquivos.
+;;
+;; English:
+;;
+;; Hexagonix and its components are licensed under a BSD-3-Clause license. Read below
+;; the license that governs this file and check each repository's license for
+;; obtain more information about your rights and obligations when using and reusing
+;; the code of this or other files.
+;;
+;;*************************************************************************************************
+;;
+;; BSD 3-Clause License
+;;
+;; Copyright (c) 2015-2023, Felipe Miguel Nery Lunkes
+;; All rights reserved.
+;; 
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+;; 
+;; 1. Redistributions of source code must retain the above copyright notice, this
+;;    list of conditions and the following disclaimer.
+;;
+;; 2. Redistributions in binary form must reproduce the above copyright notice,
+;;    this list of conditions and the following disclaimer in the documentation
+;;    and/or other materials provided with the distribution.
+;;
+;; 3. Neither the name of the copyright holder nor the names of its
+;;    contributors may be used to endorse or promote products derived from
+;;    this software without specific prior written permission.
+;; 
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+;; FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+;; CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+;; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;
+;; $HexagonixOS$
+
+use32
+
+;; Agora vamos criar um cabeçalho para a imagem HAPP final do aplicativo. Anteriormente,
+;; o cabeçalho era criado em cada imagem e poderia diferir de uma para outra. Caso algum
+;; campo da especificação HAPP mudasse, os cabeçalhos de todos os aplicativos deveriam ser
+;; alterados manualmente. Com uma estrutura padronizada, basta alterar um arquivo que deve
+;; ser incluído e montar novamente o aplicativo, sem a necessidade de alterar manualmente
+;; arquivo por arquivo. O arquivo contém uma estrutura instanciável com definição de 
+;; parâmetros no momento da instância, tornando o cabeçalho tão personalizável quanto antes.
+
+include "HAPP.s" ;; Aqui está uma estrutura para o cabeçalho HAPP
+
+;; Instância | Estrutura | Arquitetura | Versão | Subversão | Entrada | Tipo  
+cabecalhoAPP cabecalhoHAPP HAPP.Arquiteturas.i386, 1, 00, inicioAPP, 01h
+
+;;************************************************************************************
+
+include "hexagon.s"
+include "macros.s"
+
+;;************************************************************************************
+
+versaoOOBE = "1.0"
+
+OOBE:
+
+.banner: 
+
+db "88                                                                                88", 10
+db "88                                                                                ''", 10
+db "88", 10
+db "88,dPPPba,   ,adPPPba, 8b,     ,d8 ,adPPPPba,  ,adPPPb,d8  ,adPPPba,  8b,dPPPba,  88 8b,     ,d8", 10
+db "88P'    '88 a8P     88  `P8, ,8P'  ''     `P8 a8'    `P88 a8'     '8a 88P'   `'88 88  `P8, ,8P'", 10
+db "88       88 8PP'''''''    )888(    ,adPPPPP88 8b       88 8b       d8 88       88 88    )888(", 10
+db "88       88 '8b,   ,aa  ,d8' '8b,  88,    ,88 '8a,   ,d88 '8a,   ,a8' 88       88 88  ,d8' '8b,", 10
+db "88       88  `'Pbbd8'' 8P'     `P8 `'8bbdP'P8  `'PbbdP'P8  `'PbbdP''  88       88 88 8P'     `P8", 10
+db "                                              aa,    ,88", 10
+db "                                               'P8bbdP'", 10, 10, 0
+
+.mensagemInicial:
+
+db "Welcome to Hexagonix Operating System!", 10, 10
+db "This is the first time we see you around here!", 10, 10
+db "Hexagonix is a Unix-like system built from scratch in x86 Assembly, designed to be light, simple", 10
+db "and fast.", 10, 10
+db "You must be used to Unix-like utilities, right? If not, enter the command 'ls' to see the files and", 10
+db "other utilities. Then use 'man utility' to get help on each of the other utilities available on the", 10
+db "system volume.", 10, 10
+db "It's a pleasure to have you here!", 10
+db "Now let's take you to the login prompt.", 10, 10, 0
+
+
+.mensagemFinal:
+
+db "You will no longer see this message on the next restart.", 10, 0
+
+.deletarManual:
+
+db 10, "An error occurred while removing the OOBE. Remove it manually. For this, enter 'rm oobe'.", 10, 0
+
+.arquivoOOBE: db "oobe", 0
+.root:        db "root", 0
+.jack:        db "jack", 0
+
+;;************************************************************************************
+
+inicioAPP:
+
+    hx.syscall limparTela
+
+    novaLinha
+
+    fputs OOBE.banner 
+
+    fputs OOBE.mensagemInicial
+
+;; Vamos logar automaticamente como root para escluir o oobe. Depois, vamos logar
+;; como um usuário normal, "jack".
+
+.root:
+
+    mov eax, 777 ;; Código de um usuário raiz
+    
+    mov esi, OOBE.root
+    
+    hx.syscall definirUsuario
+
+    mov esi, OOBE.arquivoOOBE
+
+    hx.syscall deletarArquivo
+
+    jc .erroDeletando
+
+    fputs OOBE.mensagemFinal
+
+    mov eax, 555 ;; Código de um usuário comum
+
+    mov esi, OOBE.jack
+    
+    hx.syscall definirUsuario
+
+    jmp .terminar
+
+.erroDeletando:
+
+    fputs OOBE.deletarManual
+
+.terminar:
+
+    hx.syscall encerrarProcesso
