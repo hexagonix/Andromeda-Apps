14f18be7600f31f7d75c63aae7f03830866685d4
Felipe Lunkes <felipenldev@gmail.com>
2023-12-22 21:39:53 -0300

    v1.3.0

--- OOBE/oobe.asm
+++ OOBE/oobe.asm
@@ -68,12 +68,12 @@
 
 use32
 
-;; Agora vamos criar um cabeçalho para a imagem HAPP final do aplicativo.
+;; Now let's create a HAPP header for the application
 
-include "HAPP.s" ;; Aqui está uma estrutura para o cabeçalho HAPP
+include "HAPP.s" ;; Here is a structure for the HAPP header
 
-;; Instância | Estrutura | Arquitetura | Versão | Subversão | Entrada | Tipo
-cabecalhoAPP cabecalhoHAPP HAPP.Arquiteturas.i386, 1, 00, inicioAPP, 01h
+;; Instance | Structure | Architecture | Version | Subversion | Entry Point | Image type
+cabecalhoAPP cabecalhoHAPP HAPP.Arquiteturas.i386, 1, 00, applicationStart, 01h
 
 ;;************************************************************************************
 
@@ -83,16 +83,16 @@ include "console.s"
 
 ;;************************************************************************************
 
-versaoOOBE = "1.2.0"
+versaoOOBE = "1.3.0"
 
-COR_LOGO      = VERDE_40
-COR_SEPARADOR = HEXAGONIX_BLOSSOM_VERMELHO
-COR_ERRO      = ROXO_ESCURO
-COR_PAGINA    = HEXAGONIX_BLOSSOM_VERDE
+COLOR_LOGO    = VERDE_40
+COLOR_DIVIDER = HEXAGONIX_BLOSSOM_VERMELHO
+COLOR_ERROR   = ROXO_ESCURO
+COLOR_PAGE    = HEXAGONIX_BLOSSOM_VERDE
 
 OOBE:
 
-.divisoria:
+.divider:
 
 db "****************************************************************************************************", 0
 
@@ -126,7 +126,7 @@ db "Page 5/6", 10, 10, 0
 .pag6:
 db 10
 db "Page 6/6", 10, 10, 0
-.pagina1:
+.page1:
 db "Welcome to Hexagonix Operating System!", 10, 10
 db "This must be your first contact with Hexagonix, right?", 10, 10
 db "Now let's take a quick tour of Hexagonix, its components and utilities.", 10, 10 ;; '
@@ -140,13 +140,13 @@ db "copyright) in other projects, commercial or not, in addition to allowing a w
 db "projects. The license also allows the software to be customized and rebuilt. Feel free to build", 10
 db "your Hexagonix!", 10, 10
 db "See the complete documentation at: https://github.com/hexagonix/Doc.", 0
-.pagina2:
+.page2:
 db "As a Unix-like system, you will find in Hexagonix a number of utilities common to this type of", 10
 db "system. Utilities such as sh, ps, top, clear, init, login, cat, cowsay, mount, free and man, among", 10
 db "others, are available in a standard system installation. Its usage syntax is based on the syntax of", 10
 db "FreeBSD utilities. Whenever you are in doubt about the syntax or function of a utility, feel free", 10
 db "to use 'man utility_name' and obtain a manual for using this utility.", 0
-.pagina3:
+.page3:
 db "In addition to the utilities common to the Unix environment, Hexagonix also comes with a number of", 10
 db "unique applications. These applications present a visual environment different from the traditional", 10
 db "Unix environment. Here you can find a calculator, a settings application, a virtual piano, graphical", 10
@@ -154,7 +154,7 @@ db "shutdown utilities, a powerful text editor and a development environment whe
 db "utilities for the system.", 10, 10
 db "This environment is referred to in the Hexagonix documentation as the Hexagonix-Andromeda", 10
 db "environment, in case you need this information later or are reading the system documentation.", 0
-.pagina4:
+.page4:
 db "In addition, Hexagonix comes with a port of the flat assembler (fasm), here called fasm Hexagonix", 10
 db "Edition, or simply fasmX. This assembler is the same one used to build all of Hexagonix, and is used", 10
 db "by the Hexagonix IDE (Lyoko) to generate the executable images of the utilities written by it. In", 10
@@ -162,7 +162,7 @@ db "the root directory of the system, you can find two sample files, gapp.asm an
 db "use fasmX to generate your respective binaries, using 'fasmx gapp.asm' or 'fasmx tapp.asm'. fasm is", 10
 db "available under a free license and its sources (and adaptations for Hexagonix) can be found in the", 10
 db "system repository on GitHub (https://github.com/hexagonix/fasmX).", 0
-.pagina5:
+.page5:
 db "Feel free to contact us to learn more about the system. To do so, use the email", 10
 db "hexagonixdev@gmail.com, send a message to @HexagonixOS on Twitter or open an issue in the Hexagonix", 10
 db "repository at https://github.com/hexagonix/hexagonix. Also, if you are an Assembly developer (or", 10
@@ -170,20 +170,20 @@ db "want to become one), C (to help develop a libc) or even fluent in another la
 db "contribute to Hexagonix! You can also contribute by helping to test the system and report bugs. For", 10
 db "all this, use the contact channels already mentioned. It will be a pleasure to have more people on", 10
 db "the project!", 0
-.pagina6:
+.page6:
 db "We have reached the end of the tutorial. When finished, enter an 'ls' in the shell to start", 10
 db "exploring the available utilities. Remember: at any time, you can use 'man utility' to learn more", 10
 db "about it. Hope you like your experience with the system!", 0
-.proximaPagina:
+.nextPage:
 db 10, 10
 db "Press any key to go to the next page...", 0
-.mensagemFinal:
+.finalMessage:
 db 10, 10
 db "You will no longer see this message on the next boot.", 10, 10
 db "Press any key to finish the tour...", 10, 10, 0 ;;'
-.deletarManual:
+.manualRemove:
 db 10, "An error occurred while removing OOBE. Remove it manually. For this, enter 'rm oobe'.", 10, 10, 0
-.arquivoOOBE:
+.fileOOBE:
 db "oobe", 0
 .root:
 db "root", 0
@@ -192,143 +192,143 @@ db "jack", 0
 
 ;;************************************************************************************
 
-inicioAPP:
+applicationStart:
 
     salvarConsole
 
-.pagina1:
+.page1:
 
-    call exibirBanner
+    call showBanner
 
-    definirCorConsole COR_PAGINA, [Lib.Console.corFundo]
+    definirCorConsole COLOR_PAGE, [Lib.Console.corFundo]
 
     fputs OOBE.pag1
 
     restaurarCorConsole
 
-    fputs OOBE.pagina1
+    fputs OOBE.page1
 
-    fputs OOBE.proximaPagina
+    fputs OOBE.nextPage
 
-    call exibirDivisoria
+    call showDivider
 
     hx.syscall aguardarTeclado
 
 
-.pagina2:
+.page2:
 
-    call exibirBanner
+    call showBanner
 
-    definirCorConsole COR_PAGINA, [Lib.Console.corFundo]
+    definirCorConsole COLOR_PAGE, [Lib.Console.corFundo]
 
     fputs OOBE.pag2
 
     restaurarCorConsole
 
-    fputs OOBE.pagina2
+    fputs OOBE.page2
 
-    fputs OOBE.proximaPagina
+    fputs OOBE.nextPage
 
-    call exibirDivisoria
+    call showDivider
 
     hx.syscall aguardarTeclado
 
-.pagina3:
+.page3:
 
-    call exibirBanner
+    call showBanner
 
-    definirCorConsole COR_PAGINA, [Lib.Console.corFundo]
+    definirCorConsole COLOR_PAGE, [Lib.Console.corFundo]
 
     fputs OOBE.pag3
 
     restaurarCorConsole
 
-    fputs OOBE.pagina3
+    fputs OOBE.page3
 
-    fputs OOBE.proximaPagina
+    fputs OOBE.nextPage
 
-    call exibirDivisoria
+    call showDivider
 
     hx.syscall aguardarTeclado
 
-.pagina4:
+.page4:
 
-    call exibirBanner
+    call showBanner
 
-    definirCorConsole COR_PAGINA, [Lib.Console.corFundo]
+    definirCorConsole COLOR_PAGE, [Lib.Console.corFundo]
 
     fputs OOBE.pag4
 
     restaurarCorConsole
 
-    fputs OOBE.pagina4
+    fputs OOBE.page4
 
-    fputs OOBE.proximaPagina
+    fputs OOBE.nextPage
 
-    call exibirDivisoria
+    call showDivider
 
     hx.syscall aguardarTeclado
 
-.pagina5:
+.page5:
 
-    call exibirBanner
+    call showBanner
 
-    definirCorConsole COR_PAGINA, [Lib.Console.corFundo]
+    definirCorConsole COLOR_PAGE, [Lib.Console.corFundo]
 
     fputs OOBE.pag5
 
     restaurarCorConsole
 
-    fputs OOBE.pagina5
+    fputs OOBE.page5
 
-    fputs OOBE.proximaPagina
+    fputs OOBE.nextPage
 
-    call exibirDivisoria
+    call showDivider
 
     hx.syscall aguardarTeclado
 
-.pagina6:
+.page6:
 
-    call exibirBanner
+    call showBanner
 
-    definirCorConsole COR_PAGINA, [Lib.Console.corFundo]
+    definirCorConsole COLOR_PAGE, [Lib.Console.corFundo]
 
     fputs OOBE.pag6
 
     restaurarCorConsole
 
-    fputs OOBE.pagina6
+    fputs OOBE.page6
 
 ;;************************************************************************************
 
 .removerOOBE:
 
-;; Vamos logar automaticamente como root para excluir o oobe. Depois, vamos logar
-;; como um usuário normal, "jack".
+;; We will automatically login as root to delete oobe. Then, let's
+;; login as a normal user, "jack".
 
 .root:
 
-    mov eax, 777 ;; Código de um usuário raiz
+    mov eax, 777 ;; Root user code
 
     mov esi, OOBE.root
 
     hx.syscall definirUsuario
 
-    mov esi, OOBE.arquivoOOBE
+    mov esi, OOBE.fileOOBE
 
     hx.syscall deletarArquivo
 
-    jc .erroDeletando
+    jc .unlinkError
 
-    fputs OOBE.mensagemFinal
+    fputs OOBE.finalMessage
 
-    definirCorConsole COR_SEPARADOR, [Lib.Console.corFundo]
+    definirCorConsole COLOR_DIVIDER, [Lib.Console.corFundo]
 
-    fputs OOBE.divisoria
+    fputs OOBE.divider
 
     restaurarCorConsole
 
-    mov eax, 555 ;; Código de um usuário comum
+    mov eax, 555 ;; Common user code
 
     mov esi, OOBE.jack
 
@@ -336,51 +336,51 @@ inicioAPP:
 
     hx.syscall aguardarTeclado
 
-    jmp terminar
+    jmp finish
 
-.erroDeletando:
+.unlinkError:
 
-    definirCorConsole COR_ERRO, [Lib.Console.corFundo]
+    definirCorConsole COLOR_ERROR, [Lib.Console.corFundo]
 
-    fputs OOBE.deletarManual
+    fputs OOBE.manualRemove
 
-    fputs OOBE.mensagemFinal
+    fputs OOBE.finalMessage
 
-    definirCorConsole COR_SEPARADOR, [Lib.Console.corFundo]
+    definirCorConsole COLOR_DIVIDER, [Lib.Console.corFundo]
 
-    fputs OOBE.divisoria
+    fputs OOBE.divider
 
     restaurarCorConsole
 
     hx.syscall aguardarTeclado
 
-    jmp terminar
+    jmp finish
 
 ;;************************************************************************************
 
-terminar:
+finish:
 
-    restaurarConsole ;; Macro que restaura o comportamento do console e limpa o terminal
+    restaurarConsole ;; Macro that restores console behavior and cleans up the console
 
     hx.syscall encerrarProcesso
 
 ;;************************************************************************************
 
-exibirBanner:
+showBanner:
 
     hx.syscall limparTela
 
-    definirCorConsole COR_SEPARADOR, [Lib.Console.corFundo]
+    definirCorConsole COLOR_DIVIDER, [Lib.Console.corFundo]
 
-    fputs OOBE.divisoria
+    fputs OOBE.divider
 
-    definirCorConsole COR_LOGO, [Lib.Console.corFundo]
+    definirCorConsole COLOR_LOGO, [Lib.Console.corFundo]
 
     fputs OOBE.banner
 
-    definirCorConsole COR_SEPARADOR, [Lib.Console.corFundo]
+    definirCorConsole COLOR_DIVIDER, [Lib.Console.corFundo]
 
-    fputs OOBE.divisoria
+    fputs OOBE.divider
 
     restaurarCorConsole
 
@@ -388,14 +388,14 @@ exibirBanner:
 
 ;;************************************************************************************
 
-exibirDivisoria:
+showDivider:
 
-    definirCorConsole COR_SEPARADOR, [Lib.Console.corFundo]
+    definirCorConsole COLOR_DIVIDER, [Lib.Console.corFundo]
 
-    novaLinha
-    novaLinha
+    putNewLine
+    putNewLine
 
-    fputs OOBE.divisoria
+    fputs OOBE.divider
 
     restaurarCorConsole
 
